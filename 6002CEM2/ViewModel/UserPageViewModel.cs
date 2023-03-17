1deb435a64fd47b6eb9cda52e3e2e9cc084fe036
Jonathan <Jonathan_P_Martin@outlook.com>
2023-03-17 17:56:01 +0000

    trail and error with getting info before load

--- 6002CEM2/ViewModel/UserPageViewModel.cs
+++ 6002CEM2/ViewModel/UserPageViewModel.cs
@@ -7,7 +7,29 @@ namespace _6002CEM2.ViewModel
     [QueryProperty("Id", "Id")]
     public partial class UserPageViewModel: ObservableObject
     {
-        [ObservableProperty]
+         public UserPageViewModel() {
+          
+        }
+        [RelayCommand]
+        async Task Load()
+        {
+            var user = await SQLService.GetUser(Int32.Parse(id));
+            UserGroup = user.group;
+            UserLocation = user.Loc;
+            UserName = "Welcome Back "+user.Username;
+            ButtonName = "Click Here to get group info";
+        }
+        
+       [ObservableProperty]
         string id;
+        [ObservableProperty]
+        int userGroup;
+        [ObservableProperty]
+        int userLocation;
+        [ObservableProperty]
+        string userName;
+        [ObservableProperty]
+        string buttonName;
+        
     }
 }
